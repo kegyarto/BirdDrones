b829246f7dbfb0a518feb2b32a9a7dfb0a344147
anaran7 <44587677+anaran7@users.noreply.github.com>
2020-02-01 01:00:59 -0600

    Added Bird model and controls to move bird

---
+++ CS426-Assgn02/Assets/Scripts/BirdController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdController : MonoBehaviour
+{
+    //https://www.youtube.com/watch?v=n29T85RuWbc
+
+    public float speed;
+
+    public Camera cam;
+    public float rotationSpeed;
+    public float rotX;
+    public float rotY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 10f;
+        rotationSpeed = 100f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //move player left right up down
+        transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * speed, 0f, Input.GetAxis("Vertical") * Time.deltaTime * speed);
+
+        //rotate camera based on mouse input
+        rotX -= Input.GetAxis("Mouse Y") * Time.deltaTime * rotationSpeed;
+        rotY += Input.GetAxis("Mouse X") * Time.deltaTime * rotationSpeed;
+
+        if (rotX < -10)
+        {
+            rotX = -10;
+        }
+        else if (rotX > 15)
+        {
+            rotX = 15;
+        }
+
+        //rotate player position based on camera postition
+        transform.rotation = Quaternion.Euler(0, rotY, 0);
+        cam.transform.rotation = Quaternion.Euler(rotX, rotY, 0);
+
+        transform.forward = cam.transform.forward;
+
+    }
+}

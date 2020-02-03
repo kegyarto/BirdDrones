7e74ee1b3eae7b610cc66bc28d948101a6bde6c6
anaran7 <44587677+anaran7@users.noreply.github.com>
2020-02-02 19:00:11 -0600

    Networking works for the player movement, shooting, and getting different birds at spawn

--- CS426-Assgn02/Assets/Scripts/BirdController.cs
+++ CS426-Assgn02/Assets/Scripts/BirdController.cs
@@ -1,14 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class BirdController : MonoBehaviour
+public class BirdController : NetworkBehaviour
 {
-    //https://www.youtube.com/watch?v=n29T85RuWbc
 
     public float speed;
 
-    public Camera cam;
+    //public Camera cam;
     public float rotationSpeed;
     public float rotX;
     public float rotY;
@@ -24,8 +24,17 @@ public class BirdController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!hasAuthority)
+        {
+            return;
+        }
+
+        Camera.main.transform.position = this.transform.position - this.transform.forward * 10 + this.transform.up * 3;
+        Camera.main.transform.LookAt(this.transform.position);
+        Camera.main.transform.parent = this.transform;
+
         //move player left right up down
-        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
         {
             transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * speed, 0f, Input.GetAxis("Vertical") * Time.deltaTime * speed);
         }
@@ -49,16 +58,23 @@ public class BirdController : MonoBehaviour
 
         //rotate player position based on camera postition
         transform.rotation = Quaternion.Euler(0, rotY, 0);
-        cam.transform.rotation = Quaternion.Euler(rotX, rotY, 0);
+        Camera.main.transform.rotation = Quaternion.Euler(rotX, rotY, 0);
 
-        transform.forward = cam.transform.forward;
+        transform.forward = Camera.main.transform.forward;
 
         //shooting bullet
         if (Input.GetButtonDown("Fire1"))
         {
-            GameObject newBullet = GameObject.Instantiate(bullet, cannon.transform.position, cannon.transform.rotation) as GameObject;
-            newBullet.GetComponent<Rigidbody>().velocity += Vector3.up * 1;
-            newBullet.GetComponent<Rigidbody>().AddForce(newBullet.transform.forward * 1500);
+            CmdSpawnMyBullet();
         }
+    }
+
+    [Command]
+    void CmdSpawnMyBullet()
+    {
+        GameObject newBullet = Instantiate(bullet, cannon.transform.position, cannon.transform.rotation);
+        NetworkServer.Spawn(newBullet);
+        newBullet.GetComponent<Rigidbody>().velocity += Vector3.up * 1;
+        newBullet.GetComponent<Rigidbody>().AddForce(newBullet.transform.forward * 1500);
     }
 }
---
+++ CS426-Assgn02/Assets/Scripts/PlayerConnection.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerConnection : NetworkBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        CmdSpawnMyBird();
+    }
+
+    public GameObject PlayerUnitPrefab1;
+    public GameObject PlayerUnitPrefab2;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+    }
+
+    [Command]
+    void CmdSpawnMyBird()
+    {
+        //give the player a fire or water bird depending on player count
+        if (NetworkServer.connections.Count % 2 == 0)
+        {
+            GameObject go = Instantiate(PlayerUnitPrefab1);
+            NetworkServer.SpawnWithClientAuthority(go, connectionToClient);
+        }
+        else
+        {
+            GameObject go = Instantiate(PlayerUnitPrefab2);
+            NetworkServer.SpawnWithClientAuthority(go, connectionToClient);
+        }
+        
+    }
+}
